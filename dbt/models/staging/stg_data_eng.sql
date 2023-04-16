9ff37dce80e42459267ae7009b6c51d948a9bee5
Antonis Stellas <a.x-stellas@hotmail.com>
2023-04-16 18:51:59 +0000

    added dbt staging

---
+++ dbt/models/staging/stg_data_eng.sql
@@ -0,0 +1,18 @@
+{{ config(materialized='view') }}
+
+
+select 
+
+cast(video_name as string) as video_name,
+cast(videoid as string) as videoid,
+cast(playlist_possition as integer) as playlist_possition,
+cast(commentCount as integer) as commentCount,
+cast(favoriteCount as integer) as favoriteCount,
+cast(likeCount as integer) as likeCount,
+cast(viewCount as integer) as viewCount,
+cast(publishedAt as timestamp) as publishedAt,
+cast(channelTitle as string) as channelTitle,
+cast(description as string) as description,
+cast(playlist_name as string) as playlist_name
+
+ from {{ source('staging','youtube_playlist_Data Engineering Zoomcamp 2023_info') }}
